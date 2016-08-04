ef3491fd1b70ff6e68e4612256afe54559a503bd
Lee <516127941@qq.com>
2016-08-04 15:42:08 +0800

    Update Pop up flash for KanColle.user.js

--- Pop up flash for KanColle.user.js
+++ Pop up flash for KanColle.user.js
@@ -9,34 +9,52 @@
 // @grant        none
 // @run-at document-idle
 // ==/UserScript==
-(function() {
+(function()
+ {
     'use strict';
     // Your code here...
     var hrefValue = window.location.href;
     if(hrefValue=='http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/')
     {
-        window.onload=(function(){
-            $("body").css({"margin":"0 0px","min-width":"800px","width":"800px","height":"480px"});
-            $('div').remove(".dmm-ntgnavi");
-            $("div#w").css({"width":"800px","height":"480px"});
-            $("div#main-ntg").css({"margin":"0 0px","padding":"0 0 0px","width":"800px","height":"480px"});
-            $("div#page").css({"width":"800px","height":"480px"});
-            $("div#area-game").css({"width":"800px","height":"480px"});
-            $("iframe#game_frame").attr("height","480");
-            $("iframe#game_frame").attr("width","800");
-            $("iframe#game_frame").css({"width":"800px","height":"480px","margin-bottom":"-4px"});//
-            $("img").remove();
-            $('div').remove(".area-pickupgame");
-            $('div').remove(".area-naviapp");
-            $('div').remove("#foot");
-            $('div').remove("#movie_frame");
-            $('div').remove("#alert");
-            $('div').remove("#block_background");
-            $('div').remove("#deqwas-collection");
-            $("img").remove();
-            $('iframe[id!="game_frame"]').remove();
-            //alert(pathname);
-        });
+        window.addEventListener('message',function(e){
+            if(e.data==='done')
+            {
+                $("body").css({"margin":"0 0px","min-width":"800px","width":"800px","height":"480px"});
+                $('div').remove(".dmm-ntgnavi");
+                $("div#w").css({"width":"800px","height":"480px"});
+                $("div#main-ntg").css({"margin":"0 0px","padding":"0 0 0px","width":"800px","height":"480px"});
+                $("div#page").css({"width":"800px","height":"480px"});
+                $("div#area-game").css({"width":"800px","height":"480px"});
+                $("iframe#game_frame").attr("height","480");
+                $("iframe#game_frame").attr("width","800");
+                $("iframe#game_frame").css({"width":"800px","height":"480px","margin-bottom":"-4px"});//
+                $("img").remove();
+                $('div').remove(".area-pickupgame");
+                $('div').remove(".area-naviapp");
+                $('div').remove("#foot");
+                $('div').remove("#movie_frame");
+                $('div').remove("#alert");
+                $('div').remove("#block_background");
+                $('div').remove("#deqwas-collection");
+                //$("img").remove();
+                //$('iframe[id!="game_frame"]').remove();
+                //alert(pathname);
+                return;
+            }
+            if(typeof e.data != 'object')
+            {
+                try
+                {
+                    var d=JSON.parse(e.data);
+                    if(d.s==='resize_iframe')
+                    {
+                        window.frames.game_frame.postMessage('go','*');
+                    }
+                }
+                catch (ex){}
+                return;
+            }
+        },false);
     }
     var host=self.location.host;
     var pathname=self.location.pathname;
@@ -50,37 +68,20 @@
             alert("");
             默认模式为Direct质量为hight。尝试修改貌似没有生效，暂时取消，可以使用FRQc插件调整至best
         });*/
-        $(window).load(function(){
-            var ef = document.getElementById("externalswf");
-            if ( !! ef) {
-                $("body").css({
-                    "width": "800px",
-                    "height": "480px"
-                });
-                $("div#spacing_top").remove();
-                $("div#adFlashWrap").remove();
-                $("div#flashWrap").next("div").remove();
-                $("div#wsFlashWrap").remove();
-                $("div#sectionWrap").remove();
-                $("a").remove();
-            }
-            else
+        window.addEventListener('message',function(e){
+            if(e.data!='go')
             {
-                var removeButton=document.createElement("input");
-                removeButton.type="button";
-                removeButton.value="删除";
-                document.getElementById("sectionWrap").parentNode.insertBefore(removeButton,document.getElementById("sectionWrap"));
-                $(removeButton).bind("click",function(){
-                    $("body").css({"width":"800px","height":"480px"});
-                    $("div#spacing_top").remove();
-                    $("div#adFlashWrap").remove();
-                    $("div#flashWrap").next("div").remove();
-                    $("div#wsFlashWrap").remove();
-                    $("div#sectionWrap").remove();
-                    $("a").remove();
-                    $("input#removeButton").remove();
-                });
+                alert(e.data);
+                return;
             }
-        });
+            $("body").css({"width":"800px","height":"480px"});
+            $("div#spacing_top").remove();
+            $("div#adFlashWrap").remove();
+            $("div#flashWrap").next("div").remove();
+            $("div#wsFlashWrap").remove();
+            $("div#sectionWrap").remove();
+            $("a").remove();
+            window.parent.postMessage('done','*');
+        },false);
     }
 })();
