da629c4565a3219e68d2a79807d52632e3449c27
Lee <516127941@qq.com>
2016-08-04 13:49:56 +0800

    Update Pop up flash for KanColle.user.js

--- Pop up flash for KanColle.user.js
+++ Pop up flash for KanColle.user.js
@@ -16,13 +16,6 @@
     if(hrefValue=='http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/')
     {
         window.onload=(function(){
-            var url;
-            for(;;)
-            {
-                url=getAPIurl();
-                if(!!url)
-                    break;
-            }
             $("body").css({"margin":"0 0px","min-width":"800px","width":"800px","height":"480px"});
             $('div').remove(".dmm-ntgnavi");
             $("div#w").css({"width":"800px","height":"480px"});
@@ -52,25 +45,42 @@
         /*
         $(document).ready(function(){
             $("embed#externalswf").attr({"wmode":"Direct","quality":"LOW","play":"false"});
+            $("embed#externalswf").attr({"wmode":"Direct","quality":"LOW","play":"false"});
+            $("embed#externalswf").attr({"wmode":"GPU","quality":"best"});
+            alert("");
+            默认模式为Direct质量为hight。尝试修改貌似没有生效，暂时取消，可以使用FRQc插件调整至best
         });*/
         $(window).load(function(){
-            $("body").css({"width":"800px","height":"480px"});
-            $("div#spacing_top").remove();
-            $("div#adFlashWrap").remove();
-            $("div#flashWrap").next("div").remove();
-            $("div#wsFlashWrap").remove();
-            $("div#sectionWrap").remove();
-            $("a").remove();
-            //$("embed#externalswf").attr({"wmode":"Direct","quality":"LOW","play":"false"});
-            //$("embed#externalswf").attr({"wmode":"GPU","quality":"best"});
-            //alert("");
-            //默认模式为Direct质量为hight。尝试修改貌似没有生效，暂时取消，可以使用FRQc插件调整至best
+            var ef = document.getElementById("externalswf");
+            if ( !! ef) {
+                $("body").css({
+                    "width": "800px",
+                    "height": "480px"
+                });
+                $("div#spacing_top").remove();
+                $("div#adFlashWrap").remove();
+                $("div#flashWrap").next("div").remove();
+                $("div#wsFlashWrap").remove();
+                $("div#sectionWrap").remove();
+                $("a").remove();
+            }
+            else
+            {
+                var removeButton=document.createElement("input");
+                removeButton.type="button";
+                removeButton.value="删除";
+                document.getElementById("sectionWrap").parentNode.insertBefore(removeButton,document.getElementById("sectionWrap"));
+                $(removeButton).bind("click",function(){
+                    $("body").css({"width":"800px","height":"480px"});
+                    $("div#spacing_top").remove();
+                    $("div#adFlashWrap").remove();
+                    $("div#flashWrap").next("div").remove();
+                    $("div#wsFlashWrap").remove();
+                    $("div#sectionWrap").remove();
+                    $("a").remove();
+                    $("input#removeButton").remove();
+                });
+            }
         });
     }
 })();
-function getAPIurl()
-{
-    var str=document.getElementsByTagName('html')[0].innerHTML;
-    str.match('src="(.*)" width="900"');
-    return  RegExp.$1;
-}
